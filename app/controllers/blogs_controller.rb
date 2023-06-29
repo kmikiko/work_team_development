aa2654a668917b0bf675642460bb51313bf97681
yuta <calendula6724yuta@gmail.com>
2023-06-29 17:22:34 +0900

    blogs

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,23 @@
+class BlogsController < ApplicationController
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+end
+
+def update
+  @blog = Blog.find(params[:id])
+  if @blog.update(blog_params)
+    redirect_to blogs_path, notice: "編集します"
+  else
+    render :edit
+  end
+end
+
+def destroy
+  @blog.destroy
+  redirect_to blogs_path, notice: "削除しました"
+end
+
+def blog_params
+  params.require(:blog).permit(:photo, :title, :content,)
+end
