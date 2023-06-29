fc411d64c171937f2b4500bde0904a5e66e56e86
Kanda Mikiko <kmikiko1121@gmail.com>
2023-06-29 16:17:17 +0900

    Fix users table and set validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_29_065655) do
+ActiveRecord::Schema.define(version: 2023_06_29_071440) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -18,8 +18,12 @@ ActiveRecord::Schema.define(version: 2023_06_29_065655) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
