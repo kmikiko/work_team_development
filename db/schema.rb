bc9303fd8fa43341906eb8ecd9c4e639626c59c3
yuko <ryoyu-fu@live.jp>
2023-06-30 17:03:05 +0900

    merge confirm

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_29_073226) do
+ActiveRecord::Schema.define(version: 2023_06_30_073300) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
