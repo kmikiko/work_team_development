eb78f61dcd75f69802d57c6c5c37ffe69b699184
Kanda Mikiko <kmikiko1121@gmail.com>
2023-06-30 16:37:42 +0900

    Create Session controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_29_072929) do
+ActiveRecord::Schema.define(version: 2023_06_30_073300) do
 
   create_table "blogs", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2023_06_29_072929) do
     t.text "photo"
     t.string "title"
     t.text "content"
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2023_06_29_072929) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "blogs", "users"
 end
